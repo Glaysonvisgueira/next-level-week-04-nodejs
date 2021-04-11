058c2be6bda97f62f94f1527182872a0ebe2bf79
glayson <glaysonwow@gmail.com>
2021-04-11 13:00:53 -0300

    SendMail HTML vote

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import { resolve } from 'path';
 
 import { SurveysRepository } from '../repositories/SurveysRepository';
 import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
@@ -15,8 +16,8 @@ class SendMailController{
         const surveysRepository = getCustomRepository(SurveysRepository);
         const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
 
-        const userAlreadyExists = await usersRepository.findOne({email});
-        if(!userAlreadyExists){
+        const user = await usersRepository.findOne({email});
+        if(!user){
             return response.status(400).json({
                 error: "User does not exists",
             })
@@ -29,15 +30,40 @@ class SendMailController{
             })
         };
 
+        const variables = {
+            name: user.name,
+            title: survey.title,
+            description: survey.description,
+            user_id: user.id,
+            link: process.env.URL_MAIL
+        }
+        const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+
+        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+            where: [{user_id: user.id}, {value: null}],
+            relations: ["user", "survey"]
+        })
+
+        if(surveyUserAlreadyExists){
+            await SendMailService.execute(email, survey.title, variables, npsPath);
+            return response.json(surveyUserAlreadyExists)
+        }
+
         const surveyUser = surveysUsersRepository.create({
-            user_id: userAlreadyExists.id,
+            user_id: user.id,
             survey_id
         })
+
+        
         await surveysUsersRepository.save(surveyUser);
-        await SendMailService.execute(email, survey.title, survey.description);
+
+        
+        
+        
+        await SendMailService.execute(email, survey.title, variables, npsPath);
         return response.json(surveyUser);
     }
-    //Vídeo aula parou em 44:00.
+    //Vídeo aula parou em 1:03:00.
 }
 
 export { SendMailController };
